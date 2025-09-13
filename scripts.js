e2f405ed4746205f5edb44814fea6fd8fb9b7089
Leticia Garcia <leticiabiscoito1020kk@gmail.com>
2025-09-12 22:53:29 -0300

    ARQUIVO js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+// ROLAGEM DA BARRA
+const input = document.getElementById("pesquisa-principal");
+    const palavras = ['Busque "Inglês"', 'Busque "Francês"', 'Busque "Canto"', 'Busque "Personal trainer"'];
+    let index = 0;
+
+    setInterval(() => {
+      index = (index + 1) % palavras.length;
+      input.setAttribute("placeholder", palavras[index]);
+    }, 2000); 
+
+
+
+function Rolagem(distancia) {
+  const barra = document.getElementById("materiasBarra");
+  barra.scrollLeft += distancia;
+}
